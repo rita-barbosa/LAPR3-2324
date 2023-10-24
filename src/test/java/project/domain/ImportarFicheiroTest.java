152a217b8c91e6fb521f6164da789d2cac7bb6c0
Matilde Varela <1220683@isep.ipp.pt>
2023-10-25 00:58:06 +0100

    G73-98 [USLP02] Update dos testes das Classes de teste de ImportarFicheiro + Ficheiros de Teste

--- src/test/java/project/domain/ImportarFicheiroTest.java
+++ src/test/java/project/domain/ImportarFicheiroTest.java
@@ -1,12 +1,48 @@
 package project.domain;
 
-
 import org.junit.Test;
+import java.io.IOException;
+import static org.junit.Assert.assertEquals;
 
 public class ImportarFicheiroTest {
 
-    @Test
-    public void testImportWateringPlan() {
+private static String result;
 
+    @Test
+    public void testImportWateringPlanSuccess() {
+        try {
+            String rightFile = "src/test/java/project/testFiles/ficheiroCorreto.txt";
+            result = ImportarFicheiro.importWateringPlan(rightFile);
+            assertEquals("Success", result);
+        }catch (IOException e){
+            System.out.println("testImportWateringPlanSuccess");
+        }
+    }
+
+    @Test
+    public void testImportWateringPlanFailureWrongLines() {
+        try {
+            String wrongLinesFile = "src/test/java/project/testFiles/ficheiroLinhasErrada.txt";
+            result = ImportarFicheiro.importWateringPlan(wrongLinesFile);
+            String exceptionMessage = "ERRO: Conteúdo ficheiro não corresponde ao esperado.\n" +
+                    "As linhas com os setores a serem regados deve ter o seguinte formato:\n" +
+                    " <parcela: [A-Z], duração, regularidade: [T,I,P,3]>, por exemplo A,14,T.";
+            assertEquals(exceptionMessage, result);
+        }catch (IOException e){
+            System.out.println("testImportWateringPlanFailureWrongLines");
+        }
+    }
+
+    @Test
+    public void testImportWateringPlanFailureWrongHours() {
+        try {
+            String wrongHoursFile = "src/test/java/project/testFiles/ficheiroHoraErrada.txt";
+            result = ImportarFicheiro.importWateringPlan(wrongHoursFile);
+            String exceptionMessage = "ERRO: Conteúdo ficheiro não corresponde ao esperado.\n" +
+                    "A primeira linha deve ter as horas de rega com o seguinte formato: hh:mm, hh:mm, etc.";
+            assertEquals(exceptionMessage, result);
+        }catch (IOException e){
+            System.out.println("testImportWateringPlanFailureWrongHours");
+        }
     }
 }

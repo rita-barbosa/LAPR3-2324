ec4e799d23082a29170d667553888ed84c3a78dd
rita_barbosa <1220841@isep.ipp.pt>
2023-10-24 23:14:14 +0100

    G73-13 [US BD07] Criação de script da us-bd07.

---
+++ docs/bddad/us-bd07/script-us-bd07.sql
@@ -0,0 +1,8 @@
+SELECT p.idParcela ID_PARCELA, p.nomeParcela NOME_PARCELA,
+       oa.designacaoOperacaoAgricola TIPO_OPERACAO, COUNT(*) NUM_OPERACOES
+FROM Parcela p
+    INNER JOIN OperacaoAgricola oa ON p.idParcela = oa.idParcela
+WHERE  OA.data >= TO_DATE('01/01/2016', 'DD/MM/YYYY')
+    AND OA.data <= TO_DATE('31/12/2022', 'DD/MM/YYYY')
+GROUP BY p.idParcela,p.nomeParcela, oa.designacaoOperacaoAgricola
+ORDER BY p.idParcela;

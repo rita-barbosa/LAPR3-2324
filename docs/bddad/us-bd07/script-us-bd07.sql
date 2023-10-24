7f74ac6fd148ffb0658a5b53a538b4fa22d2c029
rita_barbosa <1220841@isep.ipp.pt>
2023-10-24 23:15:14 +0100

    G73-13 [US BD07] Reformatação do script.

--- docs/bddad/us-bd07/script-us-bd07.sql
+++ docs/bddad/us-bd07/script-us-bd07.sql
@@ -1,8 +1,10 @@
-SELECT p.idParcela ID_PARCELA, p.nomeParcela NOME_PARCELA,
-       oa.designacaoOperacaoAgricola TIPO_OPERACAO, COUNT(*) NUM_OPERACOES
+SELECT p.idParcela                   ID_PARCELA,
+       p.nomeParcela                 NOME_PARCELA,
+       oa.designacaoOperacaoAgricola TIPO_OPERACAO,
+       COUNT(*)                      NUM_OPERACOES
 FROM Parcela p
-    INNER JOIN OperacaoAgricola oa ON p.idParcela = oa.idParcela
-WHERE  OA.data >= TO_DATE('01/01/2016', 'DD/MM/YYYY')
-    AND OA.data <= TO_DATE('31/12/2022', 'DD/MM/YYYY')
-GROUP BY p.idParcela,p.nomeParcela, oa.designacaoOperacaoAgricola
+         INNER JOIN OperacaoAgricola oa ON p.idParcela = oa.idParcela
+WHERE oa.data >= TO_DATE('01/01/2016', 'DD/MM/YYYY')
+  AND oa.data <= TO_DATE('31/12/2022', 'DD/MM/YYYY')
+GROUP BY p.idParcela, p.nomeParcela, oa.designacaoOperacaoAgricola
 ORDER BY p.idParcela;

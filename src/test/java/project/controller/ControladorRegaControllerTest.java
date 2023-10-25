1243e00d8fcd4e426ffc13308ea10534cbed5e2e
ana_guterres <1221933@isep.ipp.pt>
2023-10-25 18:46:13 +0100

    G73-87 [US LP02] Atualização da classe ControladorRegaControllerTest.

--- src/test/java/project/controller/ControladorRegaControllerTest.java
+++ src/test/java/project/controller/ControladorRegaControllerTest.java
@@ -1,15 +1,9 @@
 package project.controller;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import project.domain.ImportarFicheiro;
-import project.domain.SistemaDeRega;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -19,18 +13,14 @@ import static org.junit.Assert.assertEquals;
 public class ControladorRegaControllerTest{
     private static final ControladorRegaController controller = new ControladorRegaController();
 
-    @Test
-    public void testCheckWateringInRealTime() {
-        //check
-        LocalDate date = LocalDate.now();
-        LocalTime time = LocalTime.of(8, 35);
-        String expected = "a";
-        String result;
+    @Before
+    public void setUp() throws Exception {
+        ImportarFicheiro.importWateringPlan("src/test/java/project/testFiles/ficheiroCorreto.txt");
     }
 
     @Test
     public void testCheckWateringInSimulatedTimeNotExists() {
-        LocalDate date = LocalDate.of(2025, 1, 26);
+        LocalDate date = LocalDate.now().plusDays(10000);
         LocalTime time = LocalTime.of(8, 35);
         String expected = "O dia indicado está fora do espaço de tempo ativo do plano de rega importado.";
         assertEquals(expected, controller.checkWateringInSimulatedTime(date, time));
@@ -38,34 +28,28 @@ public class ControladorRegaControllerTest{
 
     @Test
     public void testCheckWateringInSimulatedTimeExists1()  {
-        LocalDate date = LocalDate.now().plusDays(4);
-        LocalTime time = LocalTime.of(8, 35);
-        String expected = "Parcela a ser regada neste momento: D | Tempo restante: 14 minutos.\n";
-        assertEquals(expected, controller.checkWateringInSimulatedTime(date, time));
+        LocalDate date1 = LocalDate.now().plusDays(2);  //mudar o dia
+        LocalTime time1 = LocalTime.of(8, 35);
+        String expected1 = "Parcela a ser regada neste momento: D | Tempo restante: 14 minutos.\n";
+        assertEquals(expected1, controller.checkWateringInSimulatedTime(date1, time1));
+
+        LocalDate date2 = LocalDate.now().plusDays(4);
+        LocalTime time2 = LocalTime.of(8, 40);
+        String expected2 = "Parcela a ser regada neste momento: D | Tempo restante: 9 minutos.\n";
+        assertEquals(expected2, controller.checkWateringInSimulatedTime(date2, time2));
     }
 
     @Test
-    public void testCheckWateringInSimulatedTimeExists2() {
-        LocalDate date = LocalDate.now().plusDays(4);
-        LocalTime time = LocalTime.of(8, 40);
-        String expected = "Parcela a ser regada neste momento: D | Tempo restante: 9 minutos.\n";
-        assertEquals(expected, controller.checkWateringInSimulatedTime(date, time));
-    }
+    public void testCheckWateringInSimulatedTimeExists2()  {
+        LocalDate date1 = LocalDate.now().plusDays(8);
+        LocalTime time1 = LocalTime.of(17, 5);
+        String expected1 = "Parcela a ser regada neste momento: A | Tempo restante: 9 minutos.\n";
+        assertEquals(expected1, controller.checkWateringInSimulatedTime(date1, time1));
 
-    @Test
-    public void testCheckWateringInSimulatedTimeExists3()  {
-        LocalDate date = LocalDate.now().plusDays(8);
-        LocalTime time = LocalTime.of(17, 5);
-        String expected = "Parcela a ser regada neste momento: A | Tempo restante: 9 minutos.\n";
-        assertEquals(expected, controller.checkWateringInSimulatedTime(date, time));
-    }
-
-    @Test
-    public void testCheckWateringInSimulatedTimeExists4() {
-        LocalDate date = LocalDate.now().plusDays(10);
-        LocalTime time = LocalTime.of(17, 18);
-        String expected = "Parcela a ser regada neste momento: B | Tempo restante: 4 minutos.\n";
-        assertEquals(expected, controller.checkWateringInSimulatedTime(date, time));
+        LocalDate date2 = LocalDate.now().plusDays(10);
+        LocalTime time2 = LocalTime.of(17, 18);
+        String expected2 = "Parcela a ser regada neste momento: B | Tempo restante: 4 minutos.\n";
+        assertEquals(expected2, controller.checkWateringInSimulatedTime(date2, time2));
     }
 
 
@@ -89,8 +73,5 @@ public class ControladorRegaControllerTest{
         assertEquals(expected, controller.checkIfPlanIsPresent());
     }
 
-    @Before
-    public void setUp() throws Exception {
-        ImportarFicheiro.importWateringPlan("src/test/java/project/testFiles/ficheiroCorreto.txt");
-    }
+
 }

156b47ffa4b4bca9ed86a2dc501f5a5039fd6b1c
zeafo <1220738@isep.ipp.pt>
2023-10-25 22:16:52 +0100

    G73-84 [US LP02] Implementação do método checkIsWateringHourTest.

--- src/test/java/project/domain/ControladorRegaTest.java
+++ src/test/java/project/domain/ControladorRegaTest.java
@@ -8,6 +8,7 @@ import project.domain.ImportarFicheiro;
 import project.domain.SistemaDeRega;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -19,18 +20,36 @@ public class ControladorRegaTest {
     @Before
     public void setUp() throws Exception {
         ImportarFicheiro.importWateringPlan("src/test/java/project/testFiles/ficheiroCorretoV2.txt");
+        LocalDate dataInicio = LocalDate.of(2023, 10, 20);
+        SistemaDeRega.setInicioDoPlanoDeRega(dataInicio);
     }
 
     @Test
     public void checkIsWateringHour() {
 
+        LocalDate dataImpar = LocalDate.of(2023, 10, 21);
+        LocalDate dataPar = LocalDate.of(2023, 10, 22);
+        LocalDate dataDiv3 = LocalDate.of(2023, 10, 23);
+
+        LocalTime tempo815 = LocalTime.of(8,15);
+        LocalTime tempo1715 = LocalTime.of(17,15);
+        LocalTime tempoFora = LocalTime.of(1, 0);
+
+        Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringHour(tempo815, dataImpar),"Parcela a ser regada neste momento: B | Tempo restante: 7 minutos.\n" );
+        Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringHour(tempo1715, dataImpar),"Parcela a ser regada neste momento: B | Tempo restante: 7 minutos.\n");
+
+        Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringHour(tempo815, dataPar),"Parcela a ser regada neste momento: C | Tempo restante: 1 minutos.\n" );
+        Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringHour(tempo1715, dataPar),"Parcela a ser regada neste momento: C | Tempo restante: 1 minutos.\n");
+
+        Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringHour(tempo815, dataDiv3),"Parcela a ser regada neste momento: B | Tempo restante: 7 minutos.\n" );
+        Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringHour(tempo1715, dataDiv3),"Parcela a ser regada neste momento: B | Tempo restante: 7 minutos.\n");
+
+        Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringHour(tempoFora, dataImpar),"Não há parcelas a serem regadas agora.");
+
     }
 
     @Test
     public void checkIsWateringDayTest() {
-
-        LocalDate dataInicio = LocalDate.of(2023, 10, 20);
-        SistemaDeRega.setInicioDoPlanoDeRega(dataInicio);
 
         LocalDate dataImpar = LocalDate.of(2023, 10, 21);
         LocalDate dataPar = LocalDate.of(2023, 10, 22);
@@ -55,9 +74,5 @@ public class ControladorRegaTest {
         Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringDay(dataImpar), mapImpar);
         Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringDay(dataPar), mapPar);
         Assertions.assertEquals(SistemaDeRega.getControladorRega().checkIsWateringDay(dataDiv3), mapDiv3);
-    }
-
-    @Test
-    public void checkIsWateringNoData() {
     }
 }

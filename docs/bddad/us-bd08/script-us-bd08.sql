32b937e2e3969573e04cd9014ecc71c030d98ff7
zeafo <1220738@isep.ipp.pt>
2023-10-28 21:44:04 +0100

    G73-27 [US BD08] Update do script necessário á US BD08.

--- docs/bddad/us-bd08/script-us-bd08.sql
+++ docs/bddad/us-bd08/script-us-bd08.sql
@@ -4,8 +4,7 @@ WHERE FATORPRODUCAO.NOMECOMERCIAL = (SELECT NOMECOMERCIAL
                                      FROM (SELECT APLICACAOFATORPRODUCAO.NOMECOMERCIAL
 	                                       FROM APLICACAOFATORPRODUCAO INNER JOIN OPERACAO
 	                                       ON APLICACAOFATORPRODUCAO.IDOPERACAO = OPERACAO.IDOPERACAO
-                                           WHERE OPERACAO.DATAOPERACAO >= TO_DATE('01/01/2016', 'DD/MM/YYYY')
-	                                       AND OPERACAO.DATAOPERACAO <= TO_DATE('31/12/2023', 'DD/MM/YYYY'))
+                                           WHERE OPERACAO.DATAOPERACAO BETWEEN TO_DATE('01/01/2016', 'DD/MM/YYYY') AND TO_DATE('31/12/2023', 'DD/MM/YYYY'))
                                      HAVING COUNT(NOMECOMERCIAL) = (SELECT MAX(COUNT(APLICACAOFATORPRODUCAO.NOMECOMERCIAL))
 				                                                   FROM APLICACAOFATORPRODUCAO
 				 			                                       GROUP BY APLICACAOFATORPRODUCAO.NOMECOMERCIAL)

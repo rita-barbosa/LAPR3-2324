35aa4fa7a6ccdb1d417959e3d8dea42f54555a2e
rita_barbosa <1220841@isep.ipp.pt>
2023-10-29 17:39:42 +0000

    [US BD07] Alteração final script.

--- docs/bddad/sprint1/us-bd07/script-us-bd07.sql
+++ docs/bddad/sprint1/us-bd07/script-us-bd07.sql
@@ -1,20 +1,23 @@
-WITH OperacoesTotais AS (
-    SELECT p.idParcela, o.idOperacao, toa.designacaoOperacaoAgricola
-    FROM Parcela p
-             INNER JOIN OperacaoParcela op ON p.idParcela = op.idParcela
+WITH Operacoes AS (
+    SELECT DISTINCT idParcela,
+                    idOperacao
+    FROM (
+             SELECT idParcela, idOperacao
+             FROM OperacaoCultura
+             UNION ALL
+             SELECT idParcela, idOperacao
+             FROM OperacaoParcela
+         )
+),TipoOperacoes AS(
+    SELECT op.idParcela, o.idOperacao, toa.designacaoOperacaoAgricola
+    FROM Operacoes op
              INNER JOIN Operacao o ON op.idOperacao = o.idOperacao
-             INNER JOIN TipoOperacaoAgricola toa ON o.designacaoOperacaoAgricola = toa.designacaoOperacaoAgricola
-    WHERE o.dataOperacao BETWEEN TO_DATE('01/01/2016', 'DD/MM/YYYY') AND TO_DATE('31/12/2024', 'DD/MM/YYYY')
-    UNION ALL
-    SELECT oc.idParcela, o.idOperacao, toa.designacaoOperacaoAgricola
-    FROM OperacaoCultura oc
-             INNER JOIN Operacao o ON oc.idOperacao = o.idOperacao
              INNER JOIN TipoOperacaoAgricola toa ON o.designacaoOperacaoAgricola = toa.designacaoOperacaoAgricola
     WHERE o.dataOperacao BETWEEN TO_DATE('01/01/2016', 'DD/MM/YYYY') AND TO_DATE('31/12/2024', 'DD/MM/YYYY')
 )
 SELECT idParcela AS ID_PARCELA,
        designacaoOperacaoAgricola AS TIPO_OPERACAO,
        COUNT(distinct idOperacao) AS NUMERO_OPERACOES
-FROM OperacoesTotais
+FROM TipoOperacoes
 GROUP BY idParcela, designacaoOperacaoAgricola
 ORDER BY idParcela, designacaoOperacaoAgricola;

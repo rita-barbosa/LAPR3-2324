962dfb92ea8b708dd0ed8e1d444dae289654c7a3
ana_guterres <1221933@isep.ipp.pt>
2023-10-29 17:08:08 +0000

    G73-18 [US BD06] Alteração do script da USBD06.

--- docs/bddad/sprint1/us-bd06/script-us-bd06.sql
+++ docs/bddad/sprint1/us-bd06/script-us-bd06.sql
@@ -1,25 +1,24 @@
 WITH Operacoes AS (
     SELECT DISTINCT idParcela, idOperacao
     FROM (
-        SELECT idParcela, idOperacao
-        FROM OperacaoCultura
-        WHERE dataInicial >= TO_DATE('01/01/2016', 'DD/MM/YYYY')
-        AND dataInicial <= TO_DATE('31/12/2024', 'DD/MM/YYYY')
+        SELECT oc.idParcela, oc.idOperacao
+        FROM OperacaoCultura oc
         UNION ALL
-        SELECT idParcela, idOperacao
-        FROM OperacaoParcela
+        SELECT opp.idParcela, opp.idOperacao
+        FROM OperacaoParcela opp
     )
 ), Fatores AS (
-    SELECT op.idParcela as ID_PARCELA,
+    SELECT ops.idParcela as ID_PARCELA,
            f.classificacao as TIPO_FATOR_PRODUCAO
-    FROM Operacoes op
-    INNER JOIN Operacao o ON op.idOperacao = o.idOperacao
-    INNER JOIN AplicacaoFatorProducao afp ON o.idOperacao = afp.idOperacao
+    FROM Operacoes ops
+    INNER JOIN Operacao op ON ops.idOperacao = op.idOperacao
+    INNER JOIN AplicacaoFatorProducao afp ON op.idOperacao = afp.idOperacao
     INNER JOIN FatorProducao f ON afp.nomeComercial = f.nomeComercial
+    WHERE op.dataOperacao BETWEEN TO_DATE('01/01/2016', 'DD/MM/YYYY') AND TO_DATE('31/12/2024', 'DD/MM/YYYY')
 )
-SELECT f.ID_PARCELA,
-       f.TIPO_FATOR_PRODUCAO,
-       COUNT(f.TIPO_FATOR_PRODUCAO) as NUMERO_FATORES_APLICADOS
-FROM Fatores f
-GROUP BY f.ID_PARCELA, f.TIPO_FATOR_PRODUCAO
-ORDER BY f.ID_PARCELA;
+SELECT fs.ID_PARCELA,
+       fs.TIPO_FATOR_PRODUCAO,
+       COUNT(fs.TIPO_FATOR_PRODUCAO) as NUMERO_FATORES_APLICADOS
+FROM Fatores fs
+GROUP BY fs.ID_PARCELA, fs.TIPO_FATOR_PRODUCAO
+ORDER BY fs.ID_PARCELA;

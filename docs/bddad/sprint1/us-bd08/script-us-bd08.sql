92ef8b291b6c8d1c8dda12c74e7e468cb6ef9b52
zeafo <1220738@isep.ipp.pt>
2023-10-29 18:03:45 +0000

    G73-27 [US BD08] Update do script.

--- docs/bddad/sprint1/us-bd08/script-us-bd08.sql
+++ docs/bddad/sprint1/us-bd08/script-us-bd08.sql
@@ -2,14 +2,14 @@ WITH operacoesAgricolasIDs AS (
     SELECT aplicacaofatorproducao.nomecomercial
     FROM aplicacaofatorproducao
     INNER JOIN operacao ON aplicacaofatorproducao.idoperacao = operacao.idoperacao
-    WHERE operacao.dataoperacao BETWEEN TO_DATE('01/01/2016', 'DD/MM/YYYY') AND TO_DATE('31/12/2023', 'DD/MM/YYYY')
+    WHERE operacao.dataoperacao BETWEEN TO_DATE('01/01/2013', 'DD/MM/YYYY') AND TO_DATE('07/04/2020', 'DD/MM/YYYY')
 ),
 fatorMaisUsado AS (
     SELECT operacoesAgricolasIDs.nomecomercial
     FROM operacoesAgricolasIDs
-    HAVING COUNT(operacoesAgricolasIDs.nomecomercial) = (SELECT MAX(COUNT(aplicacaofatorproducao.nomecomercial))
-				                   						 FROM aplicacaofatorproducao
-				 			       						 GROUP BY aplicacaofatorproducao.nomecomercial)
+    HAVING COUNT(operacoesAgricolasIDs.nomecomercial) = (SELECT MAX(COUNT(nomecomercial))
+				                   						 FROM operacoesAgricolasIDs
+				 			       						 GROUP BY operacoesAgricolasIDs.nomecomercial)
     GROUP BY operacoesAgricolasIDs.nomecomercial
 )
 SELECT fatorproducao.nomecomercial

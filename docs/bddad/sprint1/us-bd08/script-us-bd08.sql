7446b093df685ab3e1ff0968ecef194fcfd9865c
zeafo <1220738@isep.ipp.pt>
2023-10-29 17:37:22 +0000

    G73-27 [US BD08] Update do script.

--- docs/bddad/sprint1/us-bd08/script-us-bd08.sql
+++ docs/bddad/sprint1/us-bd08/script-us-bd08.sql
@@ -1,11 +1,17 @@
-SELECT *
-FROM FATORPRODUCAO
-WHERE FATORPRODUCAO.NOMECOMERCIAL = (SELECT NOMECOMERCIAL
-                                     FROM (SELECT APLICACAOFATORPRODUCAO.NOMECOMERCIAL
-	                                       FROM APLICACAOFATORPRODUCAO INNER JOIN OPERACAO
-	                                       ON APLICACAOFATORPRODUCAO.IDOPERACAO = OPERACAO.IDOPERACAO
-                                           WHERE OPERACAO.DATAOPERACAO BETWEEN TO_DATE('01/01/2016', 'DD/MM/YYYY') AND TO_DATE('31/12/2023', 'DD/MM/YYYY'))
-                                     HAVING COUNT(NOMECOMERCIAL) = (SELECT MAX(COUNT(APLICACAOFATORPRODUCAO.NOMECOMERCIAL))
-				                                                   FROM APLICACAOFATORPRODUCAO
-				 			                                       GROUP BY APLICACAOFATORPRODUCAO.NOMECOMERCIAL)
-                                     GROUP BY NOMECOMERCIAL);
+WITH operacoesAgricolasIDs AS (
+    SELECT aplicacaofatorproducao.nomecomercial
+    FROM aplicacaofatorproducao
+    INNER JOIN operacao ON aplicacaofatorproducao.idoperacao = operacao.idoperacao
+    WHERE operacao.dataoperacao BETWEEN TO_DATE('01/01/2016', 'DD/MM/YYYY') AND TO_DATE('31/12/2023', 'DD/MM/YYYY')
+),
+fatorMaisUsado AS (
+    SELECT operacoesAgricolasIDs.nomecomercial
+    FROM operacoesAgricolasIDs
+    HAVING COUNT(operacoesAgricolasIDs.nomecomercial) = (SELECT MAX(COUNT(aplicacaofatorproducao.nomecomercial))
+				                   						 FROM aplicacaofatorproducao
+				 			       						 GROUP BY aplicacaofatorproducao.nomecomercial)
+    GROUP BY operacoesAgricolasIDs.nomecomercial
+)
+SELECT fatorproducao.nomecomercial
+FROM fatorproducao INNER JOIN fatorMaisUsado
+ON fatorproducao.nomecomercial = fatorMaisUsado.nomecomercial;

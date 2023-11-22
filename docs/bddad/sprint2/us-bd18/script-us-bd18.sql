7af7cbda0b25dea65fcc71f9c12d1a583a227ee9
rita_barbosa <1220841@isep.ipp.pt>
2023-11-22 10:48:01 +0000

    G73-109 | [US BD18] Correção da função e bloco anónimo para estar de acordo com as alterações do modelo relacional.

--- docs/bddad/sprint2/us-bd18/script-us-bd18.sql
+++ docs/bddad/sprint2/us-bd18/script-us-bd18.sql
@@ -1,52 +1,62 @@
 --FUNCTION:
-CREATE OR REPLACE FUNCTION listaOperacoes(p_nome_parcela Parcela.nomeParcela%type,
-
-                                              p_data_inicial Operacao.dataOperacao%type,
-                                              p_data_final Operacao.dataOperacao%type)
+CREATE OR REPLACE FUNCTION listaOperacoes(p_data_inicial Operacao.dataOperacao%type,
+                                          p_data_final Operacao.dataOperacao%type)
     RETURN SYS_REFCURSOR IS
     listaOperacoesParcela SYS_REFCURSOR;
 
 BEGIN
     OPEN listaOperacoesParcela FOR
-        WITH Operacoes AS (
-            SELECT  nomeParcela,
-                    idOperacao
+        WITH Operacoes AS (SELECT nomeParcela,
+                                  idOperacao
+                           FROM (SELECT nomeParcela, idOperacao
+                                 FROM OperacaoCultura
+                                 UNION ALL
+                                 SELECT nomeParcela, idOperacao
+                                 FROM OperacaoParcela))
 
-            FROM (
-                     SELECT nomeParcela, idOperacao
-                     FROM OperacaoCultura
-                     UNION ALL
-                     SELECT nomeParcela, idOperacao
-                     FROM OperacaoParcela
-                 )
-        )
-        SELECT O.idOperacao, O.designacaoOperacaoAgricola, O.quantidade, O.dataOperacao
+        SELECT OT.nomeParcela, O.designacaoOperacaoAgricola, O.quantidade, O.dataOperacao
         FROM Operacoes OT
-                 INNER JOIN  Operacao O
-                    ON OT.idOperacao = O.idOperacao
-                 INNER JOIN  TipoOperacaoAgricola TOA
-                    ON O.designacaoOperacaoAgricola = TOA.designacaoOperacaoAgricola
-        WHERE OT.idParcela = p_id_parcela
-        AND O.dataOperacao BETWEEN p_data_inicial AND p_data_final;
+                 INNER JOIN Operacao O
+                            ON OT.idOperacao = O.idOperacao
+                 INNER JOIN TipoOperacaoAgricola TOA
+                            ON O.designacaoOperacaoAgricola = TOA.designacaoOperacaoAgricola
+        WHERE O.dataOperacao BETWEEN p_data_inicial AND p_data_final
+        ORDER BY OT.nomeParcela;
 
     RETURN listaOperacoesParcela;
 END;
+/
+SET SERVEROUTPUT ON
 
 --DECLARE:
 DECLARE
-    listaOperacoesParcela SYS_REFCURSOR;
-    idOperacao Operacao.idOperacao%type;
+    listaOperacoesParcela      SYS_REFCURSOR;
+    nomeParcela                Parcela.nomeParcela%type;
     designacaoOperacaoAgricola TipoOperacaoAgricola.designacaoOperacaoAgricola%type;
-    quantidade Operacao.quantidade%type;
-    dataOperacao Operacao.dataOperacao%type;
+    quantidade                 Operacao.quantidade%type;
+    dataOperacao               Operacao.dataOperacao%type;
+    operacoesInexistente EXCEPTION;
 BEGIN
-    listaOperacoesParcela := listaOperacoes('Lameiro da Ponte',TO_DATE('2016-10-05', 'YYYY-MM-DD'),TO_DATE('2020-10-11', 'YYYY-MM-DD'));
-    LOOP
-        FETCH listaOperacoesParcela
-            INTO idOperacao, designacaoOperacaoAgricola, quantidade, dataOperacao;
-        EXIT WHEN  listaOperacoesParcela%notfound;
+    listaOperacoesParcela := listaOperacoes(TO_DATE('2016-10-05', 'YYYY-MM-DD'),
+                                            TO_DATE('2020-10-11', 'YYYY-MM-DD'));
 
-        dbms_output.put_line(idOperacao || ' - ' || designacaoOperacaoAgricola || ' - ' || quantidade || ' - ' || dataOperacao);
-    END LOOP;
+    FETCH listaOperacoesParcela INTO nomeParcela, designacaoOperacaoAgricola, quantidade, dataOperacao;
+
+    IF listaOperacoesParcela%ROWCOUNT = 0 THEN
+        RAISE operacoesInexistente;
+    ELSE
+        LOOP
+            EXIT WHEN listaOperacoesParcela%notfound;
+            dbms_output.put_line('['||nomeParcela || '] | ' || designacaoOperacaoAgricola || ' - ' || quantidade || ' - ' ||
+                                 dataOperacao);
+            FETCH listaOperacoesParcela INTO nomeParcela, designacaoOperacaoAgricola, quantidade, dataOperacao;
+        END LOOP;
+    END IF;
+
     CLOSE listaOperacoesParcela;
+
+EXCEPTION
+    WHEN operacoesInexistente THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Não existem operações no período indicado.');
 END;
+/

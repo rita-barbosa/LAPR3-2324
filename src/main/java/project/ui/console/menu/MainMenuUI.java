29d05dc7163d29d147243e0281844279f48715cc
rita_barbosa <1220841@isep.ipp.pt>
2023-11-24 11:45:31 +0000

    [GERAL] Organização dos menus.

--- src/main/java/project/ui/console/menu/MainMenuUI.java
+++ src/main/java/project/ui/console/menu/MainMenuUI.java
@@ -1,7 +1,5 @@
 package project.ui.console.menu;
 
-import project.ui.console.DatabaseConnectionTestUI;
-import project.ui.console.SelectStatementTestUI;
 import project.ui.console.utils.Utils;
 
 import java.util.ArrayList;
@@ -20,11 +18,10 @@ public class MainMenuUI implements Runnable {
      */
     public void run() {
         List<MenuItem> options = new ArrayList<MenuItem>();
-        options.add(new MenuItem("Testar conexão à Base de Dados", new DatabaseConnectionTestUI()));
         options.add(new MenuItem("Sistema de Rega", new MenuSistemaDeRega()));
-        options.add(new MenuItem("Rede de Distruibuiçao", new MenuRedeDistribuicao()));
-        options.add(new MenuItem("Testar SELECT", new SelectStatementTestUI()));
         options.add(new MenuItem("Registo de Operações", new MenuRegistoOperacoes()));
+        options.add(new MenuItem("Rede de Distruibuição", new MenuRedeDistribuicao()));
+        options.add(new MenuItem("Gerenciamento",new MenuManagement()));
         int option = 0;
         do {
             option = Utils.showAndSelectIndex(options, "\n\nMain Menu");
---
+++ src/main/java/project/ui/console/menu/MenuManagement.java
@@ -0,0 +1,27 @@
+package project.ui.console.menu;
+
+import project.ui.console.DatabaseConnectionTestUI;
+import project.ui.console.SelectStatementTestUI;
+import project.ui.console.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuManagement implements Runnable {
+    public MenuManagement(){
+
+    }
+    public void run() {
+        List<MenuItem> options = new ArrayList<MenuItem>();
+        options.add(new MenuItem("Testar conexão à Base de Dados", new DatabaseConnectionTestUI()));
+        options.add(new MenuItem("Testar SELECT", new SelectStatementTestUI()));
+        int option = 0;
+        do {
+            option = Utils.showAndSelectIndex(options, "\n\nMenu gerenciamento:");
+            if ((option >= 0) && (option < options.size())) {
+                options.get(option).run();
+            }
+        } while (option != -1);
+
+    }
+}

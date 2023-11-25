08b82701b0bc0eca8a6562d1fa18f4ebec558656
ana_guterres <1221933@isep.ipp.pt>
2023-11-25 23:13:03 +0000

    G73-137 #time 3h 15m [US BD17] Atualização da função e do bloco anónimo para estarem de acordo com o novo modelo relacional e adicionar teste.

--- docs/bddad/sprint2/us-bd17/script-us-bd17.sql
+++ docs/bddad/sprint2/us-bd17/script-us-bd17.sql
@@ -1,98 +1,124 @@
---COMANDOS:
-WITH Operacoes AS (
-SELECT DISTINCT idParcela, idOperacao
-    FROM (
-        SELECT oc.idParcela, oc.idOperacao
-        FROM OperacaoCultura oc
-        UNION ALL
-        SELECT opp.idParcela, opp.idOperacao
-        FROM OperacaoParcela opp
-    )
-), Fatores AS (
-    SELECT ops.idParcela as ID_PARCELA,
-           cq.formulaQuimica as COMPONENTE,
-           cq.quantidade as QUANTIDADE
-    FROM Operacoes ops
-    INNER JOIN Operacao op ON ops.idOperacao = op.idOperacao
-    INNER JOIN AplicacaoFatorProducao afp ON op.idOperacao = afp.idOperacao
-    INNER JOIN FatorProducao f ON afp.nomeComercial = f.nomeComercial
-    INNER JOIN ConstituicaoQuimica cq ON f.idFichaTecnica = cq.idFichaTecnica
-    WHERE op.dataOperacao BETWEEN TO_DATE('12/10/2010', 'DD/MM/YYYY') AND TO_DATE('26/05/2024', 'DD/MM/YYYY')
-)
-SELECT fs.ID_PARCELA,
-    fs.COMPONENTE,
-    SUM(fs.quantidade) as QUANTIDADES
-FROM Fatores fs
-GROUP BY fs.ID_PARCELA, fs.COMPONENTE
-ORDER BY fs.ID_PARCELA;
-
-
-
---FUNCTION:
-CREATE OR REPLACE FUNCTION fncListaComponentesFatores(listaParametros OUT SYS_REFCURSOR)
+-----------------------------------------------
+--FUNÇÃO---------------------------------------
+CREATE OR REPLACE FUNCTION fncListaComponentesFatores(p_nome_parcela Parcela.nomeParcela%TYPE,
+                                                      p_data_inicial Operacao.dataOperacao%TYPE,
+                                                      p_data_final Operacao.dataOperacao%TYPE)
     RETURN SYS_REFCURSOR IS
     listaComponentesParcela SYS_REFCURSOR;
 
 BEGIN
-    OPEN listaComponentesParcela FOR
+OPEN listaComponentesParcela FOR
         WITH Operacoes AS (
-            SELECT DISTINCT idParcela, idOperacao
+            SELECT DISTINCT nomeParcela, idOperacao
             FROM (
-                SELECT oc.idParcela, oc.idOperacao
+                SELECT oc.nomeParcela, oc.idOperacao
                 FROM OperacaoCultura oc
                 UNION ALL
-                SELECT opp.idParcela, opp.idOperacao
+                SELECT opp.nomeParcela, opp.idOperacao
                 FROM OperacaoParcela opp
             )
         ), Fatores AS (
-            SELECT ops.idParcela as ID_PARCELA,
+            SELECT ops.nomeParcela as NOME_PARCELA,
+                   op.quantidade as QUANTIDADE_FATOR,
+     			   afp.nomeComercial as NOME_COMERCIAL,
                    cq.formulaQuimica as COMPONENTE,
                    cq.quantidade as QUANTIDADE
             FROM Operacoes ops
             INNER JOIN Operacao op ON ops.idOperacao = op.idOperacao
             INNER JOIN AplicacaoFatorProducao afp ON op.idOperacao = afp.idOperacao
             INNER JOIN FatorProducao f ON afp.nomeComercial = f.nomeComercial
-            INNER JOIN ConstituicaoQuimica cq ON f.idFichaTecnica = cq.idFichaTecnica
-            WHERE op.dataOperacao BETWEEN TO_DATE('12/10/2010', 'DD/MM/YYYY') AND TO_DATE('26/05/2024', 'DD/MM/YYYY')
+            INNER JOIN ConstituicaoQuimica cq ON f.nomeComercial = cq.nomeComercial
+            WHERE ops.nomeParcela = p_nome_parcela
+            AND op.dataOperacao BETWEEN p_data_inicial AND p_data_final
         )
-        SELECT fs.ID_PARCELA,
-               fs.COMPONENTE,
-               SUM(fs.quantidade) as QUANTIDADES
-        FROM Fatores fs
-        GROUP BY fs.ID_PARCELA, fs.COMPONENTE
-        ORDER BY fs.ID_PARCELA;
+SELECT fs.NOME_PARCELA,
+       fs.NOME_COMERCIAL,
+       fs.COMPONENTE,
+       SUM((fs.QUANTIDADE * fs.QUANTIDADE_FATOR) / 100) as QUANTIDADE_ELEMENTO
+FROM Fatores fs
+GROUP BY fs.NOME_PARCELA, fs.NOME_COMERCIAL, fs.COMPONENTE
+ORDER BY fs.NOME_PARCELA, fs.NOME_COMERCIAL, fs.COMPONENTE;
 
-    RETURN listaComponentesParcela;
+RETURN listaComponentesParcela;
 END;
-
-
-SET SERVEROUTPUT  ON;
-
---DECLARE:
+/
+-----------------------------------------------
+-----------------------------------------------
+SET SERVEROUTPUT ON
+-----------------------------------------------
+--BLOCO ANONIMO--------------------------------
 DECLARE
-    aplicacaoFatorProducaoInexistente EXCEPTION;
-    listaComponentesParcela SYS_REFCURSOR;
-    v_id_Parcela OperacaoParcela.idParcela%type;
-    v_componente ConstituicaoQuimica.formulaQuimica%type;
-    v_quantidade ConstituicaoQuimica.quantidade%type;
+aplicacaoFatorProducaoInexistente   EXCEPTION;
+    listaComponentesParcela             SYS_REFCURSOR;
+    v_nome_Parcela                      Parcela.nomeParcela%type;
+	v_nome_comercial                    AplicacaoFatorProducao.nomeComercial%type;
+    v_componente                        ConstituicaoQuimica.formulaQuimica%type;
+    v_quantidade                        ConstituicaoQuimica.quantidade%type;  --CHECK SE ESTA QUANTIDADE PODE SER DE CONSTITUICAOQUIMICA OU SE TEM DE SER DE OPERACAO
 BEGIN
-    listaComponentesParcela := fncListaComponentesFatores(listaComponentesParcela);
+    listaComponentesParcela := fncListaComponentesFatores('Campo Novo',TO_DATE('2016-10-05', 'YYYY-MM-DD'), TO_DATE('2023-10-11', 'YYYY-MM-DD'));
 
-    FETCH listaComponentesParcela INTO v_id_Parcela, v_componente, v_quantidade;
+FETCH listaComponentesParcela INTO v_nome_Parcela, v_nome_comercial, v_componente, v_quantidade;
 
-    IF listaComponentesParcela%ROWCOUNT = 0 THEN
+IF listaComponentesParcela%ROWCOUNT = 0 THEN
         RAISE  aplicacaoFatorProducaoInexistente;
-    ELSE
+ELSE
+        DBMS_OUTPUT.PUT_LINE('PARCELA: ' || v_nome_parcela);
+        DBMS_OUTPUT.PUT_LINE( RPAD('NOME COMERCIAL',28, ' ') || '|  ' || RPAD('COMPONENTE',20, ' ') ||  '|  ' || RPAD('QUANTIDADE (kg)',20, ' '));
         LOOP
-            DBMS_OUTPUT.PUT_LINE(v_id_Parcela || ' - ' || v_componente || ' - ' || v_quantidade);
-            FETCH listaComponentesParcela
-                INTO v_id_Parcela, v_componente, v_quantidade;
-            EXIT WHEN  listaComponentesParcela%NOTFOUND;
-        END LOOP;
-    END IF;
-    CLOSE listaComponentesParcela;
+DBMS_OUTPUT.PUT_LINE(RPAD(v_nome_comercial,28, ' ') || '|  ' || RPAD(v_componente,20, ' ') ||  '|  ' || RPAD(v_quantidade,20, ' '));
+FETCH listaComponentesParcela
+    INTO v_nome_Parcela, v_nome_comercial, v_componente, v_quantidade;
+EXIT WHEN  listaComponentesParcela%NOTFOUND;
+END LOOP;
+END IF;
+CLOSE listaComponentesParcela;
 
 EXCEPTION
     WHEN aplicacaoFatorProducaoInexistente THEN
         RAISE_APPLICATION_ERROR(-20001, 'Não existem aplicações de fator produção no período indicado.');
 END;
+/
+
+
+-----------------------------------------------
+--TESTE----------------------------------------
+-- Verifica se a função retorna os componentes quimicos e as suas quantidade para a parcela selecionada e entre as datas selecionadas.
+DECLARE
+testeFail                EXCEPTION;
+    listaComponentesParcela  SYS_REFCURSOR;
+    v_nome_Parcela           Parcela.nomeParcela%type;
+	v_nome_comercial         AplicacaoFatorProducao.nomeComercial%type;
+    v_componente             ConstituicaoQuimica.formulaQuimica%type;
+    v_quantidade             ConstituicaoQuimica.quantidade%type;
+    teste_nome_Parcela       Parcela.nomeParcela%type;
+    teste_data_inicial       Operacao.dataOperacao%type;
+    teste_data_final         Operacao.dataOperacao%type;
+BEGIN
+    v_nome_Parcela := 'Campo Novo';
+    teste_nome_Parcela := 'Campo Novo';
+    teste_data_inicial := TO_DATE('2016-10-05', 'YYYY-MM-DD');
+    teste_data_final :=  TO_DATE('2023-10-11', 'YYYY-MM-DD');
+    listaComponentesParcela := fncListaComponentesFatores('Campo Novo',TO_DATE('2016-10-05', 'YYYY-MM-DD'), TO_DATE('2023-10-11', 'YYYY-MM-DD'));
+
+FETCH listaComponentesParcela INTO v_nome_Parcela, v_nome_comercial, v_componente, v_quantidade;
+
+IF listaComponentesParcela%ROWCOUNT = 0 THEN
+        RAISE testeFail;
+ELSE
+        LOOP
+            EXIT WHEN listaComponentesParcela%NOTFOUND;
+            IF(v_nome_Parcela != teste_nome_Parcela) THEN
+                RAISE testeFail;
+END IF;
+FETCH listaComponentesParcela
+    INTO v_nome_Parcela, v_nome_comercial, v_componente, v_quantidade;
+END LOOP;
+        DBMS_OUTPUT.PUT_LINE('TESTE PASSOU');
+END IF;
+CLOSE listaComponentesParcela;
+
+EXCEPTION
+    WHEN testeFail THEN
+    DBMS_OUTPUT.PUT_LINE('TESTE FALHOU');
+END;
+/

6ca00fdfe1f9e33a582db8d5964f8a6bcbdc6980
zeafo <1220738@isep.ipp.pt>
2023-11-25 20:20:09 +0000

    G73-163 #time 5h [US BD19] Implementação da funcionalidade.

---
+++ docs/bddad/sprint2/us-bd19/script-us-bd19.sql
@@ -0,0 +1,62 @@
+SET SERVEROUTPUT ON;
+
+-----------------------Obter os Dados-----------------------
+CREATE OR REPLACE FUNCTION obterAplicacaoFatoresProducao(
+    dataInicial IN operacao.dataoperacao%TYPE,
+    dataFinal IN operacao.dataoperacao%TYPE
+) RETURN SYS_REFCURSOR IS
+    listaAplicacaoFatoresProducao SYS_REFCURSOR;
+BEGIN
+    OPEN listaAplicacaoFatoresProducao FOR
+        WITH allops AS (
+            SELECT idoperacao, nomeparcela, nomecomum, variedade
+            FROM operacaocultura
+            UNION
+            SELECT idoperacao, nomeparcela, 'Todas' AS nomecomum, 'Todas' AS variedade
+            FROM operacaoparcela
+        )
+        SELECT fp.classificacao, afp.nomecomercial, allop.nomeparcela, allop.nomecomum, allop.variedade, op.dataoperacao
+        FROM aplicacaofatorproducao afp
+        INNER JOIN allops allop ON allop.idoperacao = afp.idoperacao
+        INNER JOIN operacao op ON op.idoperacao = afp.idoperacao
+        INNER JOIN fatorproducao fp ON fp.nomecomercial = afp.nomecomercial
+        WHERE op.dataoperacao BETWEEN dataInicial AND dataFinal
+        ORDER BY fp.classificacao, op.dataoperacao;
+
+    RETURN listaAplicacaoFatoresProducao;
+END;
+/
+
+-----------------------Mostrar os Dados--------------------------
+DECLARE
+    listaFatoresParcela SYS_REFCURSOR;
+    a_classificacao fatorproducao.classificacao%TYPE;
+    a_nomecomercial aplicacaofatorproducao.nomecomercial%TYPE;
+    a_nomeparcela operacaocultura.nomeparcela%TYPE;
+    a_nomecomum operacaocultura.nomecomum%TYPE;
+    a_variedade operacaocultura.variedade%TYPE;
+    a_dataoperacao operacao.dataoperacao%TYPE;
+
+    aplicacaoFatorProducaoInexistente EXCEPTION;
+BEGIN
+    listaFatoresParcela := obterAplicacaoFatoresProducao(TO_DATE('2016-10-05', 'YYYY-MM-DD'), TO_DATE('2020-10-11', 'YYYY-MM-DD'));
+
+    FETCH listaFatoresParcela INTO a_classificacao, a_nomecomercial, a_nomeparcela, a_nomecomum, a_variedade, a_dataoperacao;
+
+    IF listaFatoresParcela%ROWCOUNT = 0 THEN
+        RAISE aplicacaoFatorProducaoInexistente;
+    ELSE
+        DBMS_OUTPUT.PUT_LINE(' | Classificacao | Nome Comercial | Nome Parcela | Nome Comum  | Variedade | ' || a_dataoperacao|| ' | ');
+        LOOP
+            DBMS_OUTPUT.PUT_LINE(' | ' ||a_classificacao || ' | ' || a_nomecomercial || ' | ' || a_nomeparcela || ' | ' || a_nomecomum || ' | ' || a_variedade || ' | ' || a_dataoperacao|| ' | ');
+            FETCH listaFatoresParcela INTO a_classificacao, a_nomecomercial, a_nomeparcela, a_nomecomum, a_variedade, a_dataoperacao;
+            EXIT WHEN listaFatoresParcela%NOTFOUND;
+        END LOOP;
+    END IF;
+
+    CLOSE listaFatoresParcela;
+
+EXCEPTION
+    WHEN aplicacaoFatorProducaoInexistente THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Não existem aplicações de fator produção no período indicado.');
+END;
